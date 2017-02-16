c3e5d7555de830ca0a45f0e365ca869c6aeff88f
Johan Gustavsson <johan.gustavsson@valtech.se>
2017-02-16 21:17:40 +0100

    Reading port from ENV variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
