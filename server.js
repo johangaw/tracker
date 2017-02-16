aa56b7f45c61f598ae531d5db73bf86cf8856775
Johan Gustavsson <johan.gustavsson@valtech.se>
2017-02-16 20:54:56 +0100

    Added server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api/v1', router);
+
+router.route('/positions')
+    .get((req, res) => {
+        res.json({ data: 'Hello World!' });
+    })
+    .post((req, res) => {
+        res.json({ data: req.body.name });
+    });
+
+
+
+// Launch server
+app.listen(port, () => {
+    console.log('Example app listening on port ' + port);
+})
