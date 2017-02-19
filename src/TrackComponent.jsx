38d7a1e7dff7a9ac25f8981e739fe136965c1eed
Johan Gustavsson <johan.gustavsson@valtech.se>
2017-02-19 03:11:30 +0100

    Added request header

--- src/TrackComponent.jsx
+++ src/TrackComponent.jsx
@@ -11,6 +11,7 @@ class TrackComponent extends React.Component {
     const position = {long: 123.123, lat: 345.345, time: 890}
     let client = new XMLHttpRequest();
     client.open('POST', '/api/v1/positions', true);
+    client.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
     client.send(JSON.stringify(position));
   }
   
